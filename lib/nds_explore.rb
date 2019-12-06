a3f89f6229d528af0c50a861a50082ce557c89c6
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-12-06 05:34:26 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -31,9 +31,9 @@ def print_first_directors_movie_titles
   i = 0 
   while i < spielbergs_movies.count do 
   puts spielbergs_movies[i][:title]
-  i += 1 
-      binding.pry
+        binding.pry
 
+  i += 1 
 end 
 end 
   
